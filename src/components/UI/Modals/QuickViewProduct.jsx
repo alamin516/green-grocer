c2279cc7148d4afa357455033e1c9e2ed6726f67
tonmoy <tonmoy3673@gmail.com>
2024-09-09 00:03:41 +0600

    :tada: set modal

--- src/components/UI/Modals/QuickViewProduct.jsx
+++ src/components/UI/Modals/QuickViewProduct.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const QuickViewProduct = () => {
     return (
-        <div className='absolute w-[80%] lg:min-h-[500px]'>
+        <div className='absolute w-full min-h-screen bg-[#000]/70 top-0 left-0 z-[99999]'>
             <h2>Quick view</h2>
         </div>
     );
