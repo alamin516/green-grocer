5bdc8b88cf1e837deba31615189aeb21747c7eb1
tonmoy <tonmoy3673@gmail.com>
2024-09-08 23:47:48 +0600

    :tada: created quick view file

---
+++ src/components/UI/Modals/QuickViewProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuickViewProduct = () => {
+    return (
+        <div className='absolute w-[80%] lg:min-h-[500px]'>
+            <h2>Quick view</h2>
+        </div>
+    );
+};
+
+export default QuickViewProduct;
