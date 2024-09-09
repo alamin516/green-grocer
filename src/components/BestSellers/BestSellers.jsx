c84bcdbda4c210af6e8d4a45410eded0db0348c9
tonmoy <tonmoy3673@gmail.com>
2024-09-10 00:24:08 +0600

    :bulb: create best sellers card

--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -1,9 +1,12 @@
+import { Add, FavoriteBorder, RemoveRedEye, Sync } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
+import Ratings from "../Common/Ratings";
 
 const BestSellers = () => {
   const [products, setProducts] = useState([]);
+  const [openQuickView,setOpenQuickView]=useState(false);
 
   useEffect(() => {
     const dataFetch = async () => {
@@ -35,8 +38,8 @@ const BestSellers = () => {
         {/* ========== Best Sellers Slider =========== */}
         <Slider {...settings} className="px-[15px]">
           {products.map((product, i) => {
-            // const hasAttributes =
-            //   product?.attributes && Object.keys(product.attributes).length > 0;
+            const hasAttributes =
+              product?.attributes && Object.keys(product.attributes).length > 0;
 
             const isStock = product.stock == 0;
 
@@ -47,7 +50,7 @@ const BestSellers = () => {
                 <div className="grid grid-cols-2 border border-[#e5e5e5] mx-[15px]">
                   <div>
                     <figure className="product-figure relative">
-                      <Link to="" className="inline-block">
+                      <Link to="" className="inline-block py-5 px-[15px]">
                         {hasDiscount && (
                           <span
                             className={`bg-[#fa9f00] rounded-r-full text-white text-[13px]  leading-6 pl-[7px] pr-[11px] py-0 absolute top-5 left-0 z-10`}
@@ -83,7 +86,99 @@ const BestSellers = () => {
                       <span className="absolute left-0 bottom-0 right-0 border-b-4 border-double border-[#e5e5e5]"></span>
                     </figure>
                   </div>
-                  <div></div>
+                  <div className="p-5">
+                    <p>Brand</p>
+                    <h2>{product.name}</h2>
+                    <p>Lorem ipsum dolor sit amet....</p>
+                    {/* ============ Rating =========== */}
+                    <div className="mb-2.5">
+                      <span className="inline-block w-full">
+                        <Ratings rating={product?.rating} />
+                      </span>
+                    </div>
+                    {/* ============= Price =========== */}
+                    <div className="product-price px-2.5 mb-2.5 ">
+                      {hasDiscount ? (
+                        <>
+                          <div className="inline-block">
+                            <span className="text-base text-[#008459] leading-[18px] font-semibold">
+                              ${product.discount_price}
+                            </span>
+                          </div>
+                          <div className="inline-block pl-1">
+                            <span className="text-sm text-[#777] align-text-top line-through leading-4">
+                              ${product.price}
+                            </span>
+                          </div>
+                        </>
+                      ) : (
+                        <div className="inline-block">
+                          <span className="text-base text-[#008459] leading-[18px] font-semibold">
+                            ${product.price}
+                          </span>
+                        </div>
+                      )}
+                    </div>
+
+                    {/* ================ In-stock ========= */}
+                    <div className="mb-2.5">
+                        <span className="italic">Available : {product.stock}</span>
+                        <span className="w-full h-2.5 rounded-full bg-[#fed700] inline-block"></span>
+                    </div>
+
+                    {/* ========== add to cart ========= */}
+                    <div
+                    className="add-to-cart  transition-all
+                      duration-500 ease-in-out"
+                  >
+                    {!isStock ? (
+                      <Link
+                        to={""}
+                        className="add-cart-link block w-full h-9 leading-9 overflow-hidden bg-[#008459] mx-auto align-middle text-center  text-white hover:w-full hover:bg-[#fa9f00] transition-all duration-500 ease-in-out relative z-1 font-bold"
+                      >
+                        <span className="!font-bold mx-1 transition-all duration-500 ease-in-out">
+                          <Add style={{ height: "21px" }} />
+                        </span>
+                        <span className=" opacity-100">
+                          {hasAttributes ? "Choose options" : "Add to cart"}
+                        </span>
+                      </Link>
+                    ) : (
+                      <Link
+                        to={""}
+                        className="add-cart-link block w-full h-9 leading-9 overflow-hidden bg-[#008459] mx-auto align-middle text-center  text-white hover:w-full hover:bg-[#fa9f00] transition-all duration-500 ease-in-out relative z-1 font-bold"
+                      >
+                        <span className="!font-bold mx-1 transition-all duration-500 ease-in-out">
+                          <Add style={{ height: "21px" }} />
+                        </span>
+                        <span className="opacity-100">
+                          Out of Stock
+                        </span>
+                      </Link>
+                    )}
+                  </div>
+                  {/* ========== Icons ============== */}
+                  <div className=" bg-white h-[46px]  w-full pt-[5px] m-0 border-t border-[#e5e5e5] opacity-100 transition-all duration-500 ease-in-out">
+                    <div className="relative w-[120px] mx-auto">
+                      <button className="w-10 h-9 leading-9 hover:text-[#111] text-[21px] text-[#888]">
+                        <FavoriteBorder
+                          style={{ height: "21px", lineHeight: "36px" }}
+                        />
+                      </button>
+
+                      <button className="relative w-10 h-9 leading-9 hover:text-[#111] text-[21px] text-[#888] ">
+                        <Sync style={{ height: "21px", lineHeight: "36px" }} />
+                        <span className="absolute left-0 top-0 bottom-0 m-auto w-full h-[18px] border-x border-[#e5e5e5]"></span>
+                      </button>
+
+                      <button onClick={()=>setOpenQuickView(true)} className="w-10 h-9 leading-9 hover:text-[#111] text-[21px] text-[#888]">
+                        <RemoveRedEye
+                          style={{ height: "21px", lineHeight: "36px" }}
+                        />
+                      </button>
+                    </div>
+                  </div>
+                  </div>
                 </div>
               </div>
             );
